5a5b2c78fc7949baa71075178f808cdcbe048ef7
Nice <naserdin@protonmail.com>
2025-12-26 19:37:44 +0800

    chore: release v0.1.3

--- wxt.config.ts
+++ wxt.config.ts
@@ -9,4 +9,10 @@ export default defineConfig({
     permissions: ["storage"],
     default_locale: "en",
   },
+  runner: {
+    // Use a persistent profile so you stay logged into YouTube
+    chromiumArgs: ["--user-data-dir=./.wxt/user-data"],
+    // Automatically open YouTube when starting dev mode
+    startUrls: ["https://www.youtube.com"],
+  },
 });
